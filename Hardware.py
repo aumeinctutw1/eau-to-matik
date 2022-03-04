80f6e8f0662e421a1477ff5654e1c2f12a56a878
aumeinctutw1 <benno.schnabel@googlemail.com>
2022-03-04 11:12:31 +0100

    moistureSensor Class is defined, measures MoistureLevel, AvgMoistureLevel and Temperature

--- Hardware.py
+++ Hardware.py
@@ -1,39 +1,71 @@
 from gpiozero import OutputDevice
 import RPi.GPIO as GPIO
 import time
+import smbus
 from Hardware import Hardware
-from settings import RELAY_PIN, PUMP_OUPUT, MOISTURE_GPIO_PIN, GPIO_ECHO, GPIO_TRIGGER, MIN_WATER_LEVEL, MAX_WATER_LEVEL
+from settings import GET_TEMPERATURE, RELAY_PIN, PUMP_OUPUT, DEVICE_ADDRESS, SET_REFERENCE_WET, SET_REFERENCE_DRY, GET_SOIL_MOISTURE, REFERENCE_DRY, REFERENCE_WET, GPIO_ECHO, GPIO_TRIGGER, MIN_WATER_LEVEL, MAX_WATER_LEVEL
+
 
 class EauToMatik():
     def __init__(self):
         self.Pump = Pump()
         self.WaterLevelController = WaterLevelController()
-    
+
     def run(self):
         if (self.WaterLevelController.hasEnoughWater()):
             self.Pump.water()
 
-# not done yet
+
 class moistureSensor():
-    def getMoistureLevel():
-        return
+    def __init__(self):
+        # init the bus
+        DEVICE_BUS = 1
+        self.bus = smbus.SMBus(DEVICE_BUS)
+
+        # configure sensor with reference values, if not set -> default values provided by sensor
+        if REFERENCE_WET:
+            self.bus.write_word_data(DEVICE_ADDRESS, SET_REFERENCE_WET, REFERENCE_WET)
+            time.sleep(0.2)
+
+        if REFERENCE_DRY:
+            self.bus.write_word_data(DEVICE_ADDRESS, SET_REFERENCE_DRY, REFERENCE_DRY)
+            time.sleep(0.2)
+
+    # gets the directly measured value
+    def getMoistureLevel(self):
+        moisture = self.bus.read_i2c_block_data(DEVICE_ADDRESS, GET_SOIL_MOISTURE, 2)
+        measuredMoisture = round(moisture[1]/2.55, 1)
+        return measuredMoisture
+
+    # gets the avg value, last 30 seconds
+    def getAvgMoistureLevel(self):
+        moisture = self.bus.read_i2c_block_data(DEVICE_ADDRESS, GET_SOIL_MOISTURE, 2)
+        avgMoisture = round(moisture[0]/2.55, 1)
+        return avgMoisture
+
+    def getTemperature(self):
+        temp = self.bus.read_byte_data(DEVICE_ADDRESS, GET_TEMPERATURE)
+        return temp
+
 
 class Relay(OutputDevice):
     def __init__(self, pin, active_high):
         super(Relay, self).__init__(pin, active_high)
 
+
 class Pump():
     def __init__(self):
         self.relay = Hardware.Relay(RELAY_PIN, False)
         self.outputCapacity = PUMP_OUPUT
-    
+
     def water(self, volume):
         self.relay.on()
         time.sleep(self.getWateringDuration(volume))
         self.relay.off()
-    
+
     def getWateringDuration(self, volume):
         return volume / self.outputCapacity
+
 
 class WaterLevelController():
     def __init__(self):
@@ -43,6 +75,7 @@ class WaterLevelController():
 
     def hasEnoughWater(self):
         return self.UltrasonicSensor.getDistance() < self.maxWaterLevel - self.minWaterLevel
+
 
 class UltrasonicSensor(GPIO):
     def __init__(self):
@@ -56,26 +89,26 @@ class UltrasonicSensor(GPIO):
     def getDistance(self):
         # set Trigger to HIGH
         GPIO.output(self.triggerPin, True)
-    
+
         # set Trigger after 0.01ms to LOW
         time.sleep(0.00001)
         GPIO.output(self.triggerPin, False)
-    
+
         StartTime = time.time()
         StopTime = time.time()
-    
+
         # save StartTime
         while GPIO.input(self.echoPin) == 0:
             StartTime = time.time()
-    
+
         # save time of arrival
         while GPIO.input(self.echoPin) == 1:
             StopTime = time.time()
-    
+
         # time difference between start and arrival
         TimeElapsed = StopTime - StartTime
         # multiply with the sonic speed (34300 cm/s)
         # and divide by 2, because there and back
         distance = (TimeElapsed * 34300) / 2
-    
+
         return distance
--- settings.py
+++ settings.py
@@ -1,15 +1,23 @@
 # pump
 RELAY_PIN = 12
-PUMP_OUPUT = 1200 / 3600 # in ml/second
+PUMP_OUPUT = 1200 / 3600  # in ml/second
 
 # ultrasonic sensor
 GPIO_ECHO = 24
 GPIO_TRIGGER = 18
 
 # moisture sensor
-MOISTURE_GPIO_PIN = 17
+REFERENCE_WET = 0
+REFERENCE_DRY = 0
+
+# moisture sensor i2c config
+DEVICE_ADDRESS = 0x55
+SET_REFERENCE_DRY = 0x44
+SET_REFERENCE_WET = 0x55
+GET_SOIL_MOISTURE = 0x76
+GET_TEMPERATURE = 0x74
 
 # personal settings
-VOLUME_TO_WATER = 200 # in ml
-MIN_WATER_LEVEL = 5 # in cm
-MAX_WATER_LEVEL = 20 # high position of ultrasonic sensor
+VOLUME_TO_WATER = 200  # in ml
+MIN_WATER_LEVEL = 5  # in cm
+MAX_WATER_LEVEL = 20  # high position of ultrasonic sensor
