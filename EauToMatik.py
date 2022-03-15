4ebae76363fbf545df00d196a1a9c557b65823f8
johan morin <johan.morin@hotmail.fr>
2022-03-15 16:35:00 +0100

    app saves data automatically

--- EauToMatik.py
+++ EauToMatik.py
@@ -1,5 +1,6 @@
 from MoistureSensor import MoistureSensor
 from WaterLevelController import WaterLevelController
+from InfluxDBExporter import InfluxDBExporter
 from Pump import Pump
 from settings import MIN_SOIL_MOISTURE_LEVEL, VOLUME_TO_WATER
 
@@ -8,13 +9,33 @@ class EauToMatik():
         self.Pump = Pump()
         self.WaterLevelController = WaterLevelController()
         self.MoistureSensor = MoistureSensor()
+        self.Exporter = InfluxDBExporter()
 
     def run(self):
-        if (self.soilIsDry() and self.WaterLevelController.hasEnoughWater(VOLUME_TO_WATER)):
-            self.Pump.water()
+        self.saveDataFromSensors()
+
+        # check if watering is necessary
+        if (
+                self.soilIsDry() and
+                self.WaterLevelController.hasEnoughWater(VOLUME_TO_WATER)
+            ):
+            # activate pump
+            self.Pump.water(VOLUME_TO_WATER)
+            # save volume of water given to plant
+            self.Exporter.exportPumpedWater(VOLUME_TO_WATER)
+
+            self.saveDataFromSensors()
+
 
     def soilIsDry(self):
         return self.MoistureSensor.getAvgMoistureLevel() < MIN_SOIL_MOISTURE_LEVEL
+
+    def saveDataFromSensors(self):
+        # save moisture and temperature in DB
+        self.Exporter.exportMoistureLevel(self.MoistureSensor.getAvgMoistureLevel())
+        self.Exporter.exportSoilTemperature(self.MoistureSensor.getSoilTemperature())
+        # save water level in tank
+        self.Exporter.exportWaterLevel(self.WaterLevelController.getWaterLevelInProcent())
 
 
 
